daaa422065c139543b02c3c54430f9ff9b038c73
arthurbatista04 <arthurbatista04@outlook.com>
2020-10-26 21:45:35 -0300

    with allocation problem

--- parallel/samplesort.c
+++ parallel/samplesort.c
@@ -2,7 +2,7 @@
 
 int cmpfunc(const void *a, const void *b)
 {
-	return (*(double *)a - *(double *)b);
+	return (*(int *)a - *(int *)b);
 }
 
 int main(int argc, char *argv[])
@@ -13,10 +13,11 @@ int main(int argc, char *argv[])
 		sample_vec_size,
 		size;
 	int opt, rc;
-	double **buckets;
-	double *vector, *sample_vec, *local_splitter, *global_splitter;
-	int *bucket_sizes;
+	int **local_buckets, **global_buckets;
+	int *vector, *sample_vec, *local_splitter, *global_splitter;
+	int *local_bucket_sizes, *global_bucket_buffer_sizes, *displ, *global_bucket_sizes;
 	int local_splitter_element_size, global_splitter_size;
+	int count;
 	time_t t;
 	MPI_Status status;
 
@@ -60,77 +61,65 @@ int main(int argc, char *argv[])
 				abort();
 			}
 		}
-		vector = (double *)calloc(size, sizeof(double));
+		vector = (int *)calloc(size, sizeof(int));
 		srand(time(NULL));
-		printf("\n---------------INICIAL VECTOR-----------------\n");
-		for (int i = 0; i < size; i++)
+
+		while (count < size)
 		{
-			vector[i] = (double)rand() / (double)RAND_MAX;
-			printf("%f ", vector[i]);
+			int random_number = rand() % size + 1;
+			int repeated = FALSE;
+			for (int i = 0; i < size; i++)
+			{
+				if (vector[i] == random_number)
+				{
+					repeated = TRUE;
+				}
+			}
+			if (!repeated)
+			{
+				vector[count] = random_number;
+
+				count++;
+			}
 		}
-		printf("\n\n");
 	}
 
 	MPI_Bcast(&size, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
 
 	sample_vec_size = size / num_tasks;
-	sample_vec = (double *)calloc(sample_vec_size, sizeof(double));
+	sample_vec = (int *)calloc(sample_vec_size, sizeof(int));
 
-	MPI_Scatter(vector, sample_vec_size, MPI_DOUBLE, sample_vec,
-				sample_vec_size, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+	MPI_Scatter(vector, sample_vec_size, MPI_INT, sample_vec,
+				sample_vec_size, MPI_INT, MASTER, MPI_COMM_WORLD);
 
-	// printf("PROCESS %d SUB-VECTOR: ", task_id);
-	// for (int i = 0; i < sample_vec_size; i++)
-	// {
-	// 	printf("%f ", sample_vec[i]);
-	// }
-	// printf("\n\n");
-
-	qsort(sample_vec, sample_vec_size, sizeof(double), cmpfunc);
-
-	printf("PROCESS %d SUB-VECTOR: ", task_id);
-	for (int i = 0; i < sample_vec_size; i++)
-	{
-		printf("%f ", sample_vec[i]);
-	}
-	printf("\n");
+	qsort(sample_vec, sample_vec_size, sizeof(int), cmpfunc);
 
 	local_splitter_element_size = num_tasks - 1;
-	local_splitter = (double *)calloc(local_splitter_element_size, sizeof(double));
+	local_splitter = (int *)calloc(local_splitter_element_size, sizeof(int));
 	for (int i = 0; i < local_splitter_element_size; i++)
 	{
-		local_splitter[i] = sample_vec[size / num_tasks / num_tasks * (i + 1)];
+		local_splitter[i] = sample_vec[size / (num_tasks * num_tasks) * (i + 1)];
 	}
 
-	// printf("PROCESS %d SUB-VECTOR: ", task_id);
-	// for (int i = 0; i < local_splitter_element_size; i++)
-	// {
-	// 	printf("%f ", local_splitter[i]);
-	// }
-	// printf("\n");
 	global_splitter_size = local_splitter_element_size * num_tasks;
-	global_splitter = (double *)calloc(global_splitter_size, sizeof(double));
-	MPI_Gather(local_splitter, local_splitter_element_size, MPI_DOUBLE, global_splitter, local_splitter_element_size,
-			   MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+	global_splitter = (int *)calloc(global_splitter_size, sizeof(int));
+	MPI_Gather(local_splitter, local_splitter_element_size, MPI_INT, global_splitter, local_splitter_element_size,
+			   MPI_INT, MASTER, MPI_COMM_WORLD);
 	if (task_id == MASTER)
 	{
 
-		qsort(global_splitter, global_splitter_size, sizeof(double), cmpfunc);
-		printf("Global Splitter: ");
-		for (int i = 0; i < global_splitter_size; i++)
-		{
-			printf("%f ", global_splitter[i]);
-		}
+		global_splitter[global_splitter_size - 1] = INT_MAX;
+		qsort(global_splitter, global_splitter_size, sizeof(int), cmpfunc);
 	}
-	MPI_Bcast(global_splitter, global_splitter_size, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+	MPI_Bcast(global_splitter, global_splitter_size, MPI_INT, MASTER, MPI_COMM_WORLD);
 
-	buckets = (double **)malloc(sizeof(double *) * global_splitter_size);
+	local_buckets = (int **)malloc(sizeof(int *) * global_splitter_size);
 	for (int i = 0; i < global_splitter_size; i++)
 	{
-		buckets[i] = (double *)malloc(sizeof(double) * 2 * size / global_splitter_size);
+		local_buckets[i] = (int *)malloc(sizeof(int) * 2 * size / global_splitter_size);
 	}
 
-	bucket_sizes = (int *)calloc(global_splitter_size, sizeof(int));
+	local_bucket_sizes = (int *)calloc(global_splitter_size, sizeof(int));
 
 	for (int i = 0; i < sample_vec_size; i++)
 	{
@@ -139,26 +128,62 @@ int main(int argc, char *argv[])
 		{
 			if (sample_vec[i] < global_splitter[count])
 			{
-				buckets[count][bucket_sizes[count]] = sample_vec[i];
-				bucket_sizes[count]++;
+				local_buckets[count][local_bucket_sizes[count]] = sample_vec[i];
+				local_bucket_sizes[count]++;
 				count = global_splitter_size; // break
 			}
 			count++;
 		}
 	}
-	if (task_id == MASTER)
-	{
 
-		for (int i = 0; i < global_splitter_size; i++)
+	printf("PROCESS %d\n", task_id);
+	for (int i = 0; i < local_bucket_sizes[0]; i++)
+	{
+		printf("%d ", local_buckets[0][i]);
+	}
+	printf("\n");
+
+	global_bucket_buffer_sizes = (int *)calloc(global_splitter_size, sizeof(int));
+	displ = (int *)calloc(global_splitter_size, sizeof(int));
+	global_buckets = (int **)malloc(sizeof(int *) * global_splitter_size);
+	for (int i = 0; i < global_splitter_size; i++)
+	{
+		global_buckets[i] = (int *)malloc(sizeof(int) * 2 * size / global_splitter_size);
+	}
+	for (int i = 0; i < global_splitter_size; i++)
+	{
+		MPI_Gather(&local_bucket_sizes[i], 1, MPI_INT, global_bucket_buffer_sizes, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+		if (task_id == MASTER)
 		{
-			printf("\n\n %d BUCKET NUMERO %d\n\n", task_id, i);
-			for (int j = 0; j < bucket_sizes[i]; j++)
+
+			for (int j = 1; j < global_splitter_size; j++)
 			{
-				printf("%f ", buckets[i][j]);
+
+				displ[j] = displ[j - 1] + global_bucket_buffer_sizes[j - 1];
 			}
-			printf("\n\n");
+		}
+		MPI_Gatherv(local_buckets[i], local_bucket_sizes[i], MPI_INT,
+					global_buckets[i], global_bucket_buffer_sizes, displ, MPI_INT, MASTER, MPI_COMM_WORLD);
+		if (task_id == MASTER)
+		{
+			for (int j = 0; j < global_splitter_size; j++)
+			{
+				global_bucket_buffer_sizes[j] = 0;
+				displ[j] = 0;
+			}
 		}
 	}
+	// global_bucket_sizes = (int *)calloc(global_splitter_size, sizeof(int));
+	// MPI_Reduce(local_bucket_sizes, global_bucket_sizes, global_splitter_size, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	// if (task_id == MASTER)
+	// {
+	// 	printf("ROOT\n");
+	// 	for (int i = 0; i < global_bucket_sizes[0]; i++)
+	// 	{
+	// 		printf("%d ", global_buckets[0][i]);
+	// 	}
+	// 	printf("\n");
+	// }
 
 	MPI_Finalize();
 	return 0;
--- parallel/samplesort.h
+++ parallel/samplesort.h
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <limits.h>
 #include <unistd.h>
 #define MASTER 0               /* taskid of first task */
 #define FROM_MASTER 1          /* setting a message type */
 #define FROM_WORKER 2          /* setting a message type */
+#define TRUE 1
+#define FALSE 0
 
 int cmpfunc (const void * a, const void * b);
 
